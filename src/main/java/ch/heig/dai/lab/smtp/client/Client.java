cb292f47843941416c340f0bd93dea606e4dfbe3
Glodi Domingos <glodi.domingossanda@heig-vd.ch>
2023-12-04 10:50:09 +0100

    First client test pass

--- src/main/java/ch/heig/dai/lab/smtp/client/Client.java
+++ src/main/java/ch/heig/dai/lab/smtp/client/Client.java
@@ -1,5 +1,28 @@
-package ch.heig.dai.lab.smtp;
+package ch.heig.dai.lab.smtp.client;
 
+import java.io.*;
+import java.net.*;
+import static java.nio.charset.StandardCharsets.UTF_8;
 public class Client {
 
+    private final static String ADDR = "localhost";
+    private final static int PORT = 1025;
+    public void connect() {
+        try (Socket s = new Socket(ADDR, PORT);
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(s.getOutputStream(), UTF_8));
+        BufferedReader in = new BufferedReader((new InputStreamReader(s.getInputStream(), UTF_8)))) {
+            System.out.println(initialConnection(out,in));
+        } catch (IOException e) {
+            System.err.println("Client Error: " + e);
+        }
+    }
+
+    public String initialConnection(BufferedWriter out, BufferedReader in) throws IOException {
+        String line;
+        while ((line = in.readLine()) != null && !line.equals("220 e3be39763734 ESMTP")) {}
+        return line;
+    }
+    public static void main(String[] args) {
+        new Client().connect();
+    }
 }
--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -1,8 +1,25 @@
+import ch.heig.dai.lab.smtp.client.Client;
 import org.junit.Test;
+import java.io.*;
+import java.net.Socket;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class ClientTest {
 
     @Test
-    public void initialConnection() {}
+    public void initialConnection() {
+        //Create connection to mail server
+        String result = null;
+        try (Socket s = new Socket("localhost", 1025);
+             BufferedWriter out = new BufferedWriter(new OutputStreamWriter(s.getOutputStream(), UTF_8));
+             BufferedReader in = new BufferedReader((new InputStreamReader(s.getInputStream(), UTF_8)))) {
+            result = new Client().initialConnection(out,in);
+        } catch (IOException e) {
+            System.err.println("Client Error: " + e);
+        }
 
+        assertEquals("220 e3be39763734 ESMTP", result);
+
+    }
 }
