d43ff0c4540271863ad1506eb6ac8b6f2e07e562
Glodi Domingos <glodi.domingossanda@heig-vd.ch>
2023-12-07 03:09:28 +0100

    Main program done

--- src/main/java/ch/heig/dai/lab/smtp/Main.java
+++ src/main/java/ch/heig/dai/lab/smtp/Main.java
@@ -1,7 +1,39 @@
 package ch.heig.dai.lab.smtp;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import ch.heig.dai.lab.smtp.client.Client;
+import ch.heig.dai.lab.smtp.txtParser.MailParser;
+import ch.heig.dai.lab.smtp.txtParser.MessageParser;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<String> mailList = new MailParser().parseMails();
+
+        //Reception of desired number of groups
+        Scanner sc = new Scanner(System.in);
+        int nbrGroup;
+        boolean correctNbrGroup = true;
+        do {
+            System.out.println("Please enter the desired number of group");
+            while(!sc.hasNextInt()) {
+                System.err.println("Please only enter an Integer");
+                sc.next();
+            }
+            nbrGroup = sc.nextInt();
+            if ((mailList.size() / nbrGroup) > 5) {
+                System.err.println("The number of groups is too small for the set of email provided");
+            } else if ((mailList.size() / nbrGroup) < 2) {
+                System.err.println("The number of groups is too big for the set of email provided");
+            } else {
+                correctNbrGroup = false;
+            }
+        } while (correctNbrGroup);
+
+        MessageParser messageParser = new MessageParser();
+        messageParser.parseMessages();
+
+        new Client().connect(nbrGroup,mailList,messageParser.getSubjectList(), messageParser.getBodyList());
     }
 }
--- src/main/java/ch/heig/dai/lab/smtp/client/Client.java
+++ src/main/java/ch/heig/dai/lab/smtp/client/Client.java
@@ -2,106 +2,157 @@ package ch.heig.dai.lab.smtp.client;
 
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+
 import static java.nio.charset.StandardCharsets.UTF_8;
-import ch.heig.dai.lab.smtp.txtParser.MailParser;
 public class Client {
 
     private final static String END_CONNECTION = "221 Bye";
     private final static String EHLO = "EHLO ";
     private final static String CLIENT_IDENDITY = "heig-vd.ch";
     private final static String ADDR = "localhost";
-    private final static String ACCEPTED_REQUEST ="250 ";
-
-
+    private final static String ACCEPTED_REQUEST = "250 ";
+    private final static String START_DATA = "354";
+    private final static String CRLF = "\r\n";
     private final static int PORT = 1025;
-    public void connect() {
+
+    public void connect(int nbrOfGroup,
+                        ArrayList<String> mailList,
+                        ArrayList<String> subjectList,
+                        ArrayList<String> bodyList) {
         try (Socket s = new Socket(ADDR, PORT);
         BufferedWriter out = new BufferedWriter(new OutputStreamWriter(s.getOutputStream(), UTF_8));
         BufferedReader in = new BufferedReader((new InputStreamReader(s.getInputStream(), UTF_8)))) {
-            System.out.println(sessionInitiation(out, in));
-            System.out.println(clientInitiation(out, in));
-
-
+            sessionInitiation(out, in);
+            clientInitiation(out, in);
+            int random;
+            //Pass the correct group and all
+            for(int i = 0; i < nbrOfGroup; i++) {
+                ArrayList<String> subGroup = new ArrayList<>();
+                for (int idx = 0; idx < (int) (idx + Math.ceil(mailList.size()/nbrOfGroup)) && idx < mailList.size(); idx++) {
+                    subGroup.add(mailList.get(idx));
+                }
+                setSenderAndRecipients(out, in, subGroup);
+                random = ThreadLocalRandom.current().nextInt(bodyList.size());
+                setMsgBody(out,in,mailList, subjectList.get(random), bodyList.get(random));
+            }
+            endConnection(out,in);
         } catch (IOException e) {
             System.err.println("Client Error: " + e);
         }
     }
 
-    public String sessionInitiation(BufferedWriter out, BufferedReader in) throws IOException {
+    private void sessionInitiation(BufferedWriter out, BufferedReader in) throws IOException {
         String line;
         while ((line = in.readLine()) != null && !line.contains("220")) {}
 
         //In case there is an error here, the client should quit the connection
         if (line.contains("Error")) {
-            line = endConnection(out, in);
+            endConnection(out, in);
         }
-        return line;
     }
 
-    public String clientInitiation(BufferedWriter out, BufferedReader in) throws IOException {
+    private void clientInitiation(BufferedWriter out, BufferedReader in) throws IOException {
         String line;
-        out.write(EHLO + CLIENT_IDENDITY + "\r\n");
+        out.write(EHLO + CLIENT_IDENDITY + CRLF);
         out.flush();
 
         //TODO: Make it a function
         while((line = in.readLine()) != null && !line.contains(ACCEPTED_REQUEST)) {
-            System.out.println(line);
             //To deal with error that could happens in client initiation phase
             if (line.contains("Error")) {
-                line = endConnection(out, in);
+                endConnection(out, in);
                 break;
             }
         }
-        return line;
     }
 
-    public String endConnection(BufferedWriter out, BufferedReader in) throws IOException {
+    private void endConnection(BufferedWriter out, BufferedReader in) throws IOException {
         String line;
-        out.write("QUIT\r\n");
+        out.write("QUIT" + CRLF);
         out.flush();
         //TODO: Make it a function
-        while ((line = in.readLine()) != null && !line.contains(ACCEPTED_REQUEST)) {}
-        return line;
+        while ((line = in.readLine()) != null && !line.contains(END_CONNECTION)) {}
+        System.out.println("Mail Sent");
     }
 
-    public String mailAddressFormat(String mail) {
-        return "<" + utf8Formatting(mail.toLowerCase()) + ">\r\n";
+    private String mailAddressFormat(String mail) {
+        return "<" + mail.toLowerCase() + ">" + CRLF;
     }
 
-    public boolean mailTransadtions(BufferedWriter out, BufferedReader in) throws IOException {
-        String mail = "example@example.com";
+    /**
+     * Send the sender and recipient to the mail server
+     *
+     * @param out
+     * @param in
+     * @param mailList
+     * @throws IOException
+     */
+    private void setSenderAndRecipients(BufferedWriter out,
+                                       BufferedReader in,
+                                       ArrayList<String> mailList) throws IOException {
         String line;
         //Setting hidden sender
-        out.write("MAIL FROM:" + mailAddressFormat(mail));
+        out.write("MAIL FROM:" + mailAddressFormat(mailList.getFirst()));
         out.flush();
         //TODO: Make it a function
         while ((line = in.readLine()) != null && !line.contains(ACCEPTED_REQUEST)) {}
 
-        //Setting hidden recipients - FOllow RFC
-
-        for (int i = 0; i < 5; i++) {
-            out.write("RCPT TO:" + mailAddressFormat(""));
+        //Setting hidden recipients
+        for (int i = 1; i < mailList.size(); i++) {
+            out.write("RCPT TO:" + mailAddressFormat(mailList.get(i)));
             out.flush();
             //TODO: Make it a function
             while ((line = in.readLine()) != null && !line.contains(ACCEPTED_REQUEST)) {}
         }
-        return true;
     }
 
-    public boolean writeBodyMessages(BufferedWriter out, BufferedReader in) throws IOException {
+    private void setMsgBody(BufferedWriter out,
+                           BufferedReader in,
+                           ArrayList<String> mailList,
+                           String subject,
+                           String body) throws IOException {
         String line;
-        out.write("DATA");
+        out.write("DATA" + CRLF);
         out.flush();
-        //TODO: Make it a function (parameters in accepted request code)
-        while ((line = in.readLine()) != null && !line.equals("354 ")) {}
+        //TODO - Make it a function consume text until ...
+        while ((line = in.readLine()) != null && !line.contains(START_DATA)) {}
 
-        return false;
-    }
+        //Setting Body encoding to UTF-8
+        out.write("Content-Type: text/plain; charset=utf-8" + CRLF);
+        out.flush();
 
-    public String utf8Formatting(String text) {
+        //Setting visible sender
+        out.write("From: " + mailAddressFormat(mailList.getFirst()));
+        out.flush();
+        StringBuilder visibleRecipients = new StringBuilder();
+        for (int i = 1; i < mailList.size(); i++) {
+            visibleRecipients.append(mailList.get(i)).append(";");
+        }
+
+        //Set visible recipients
+        out.write("To: " + visibleRecipients.substring(0,visibleRecipients.toString().length() -1).toLowerCase() + CRLF);
+        out.flush();
+
+
+        //Setting the Subject
+        out.write("Subject: " + utf8Formatting(subject) + CRLF);
+        out.flush();
+
+        //Setting the Body
+        out.write( CRLF);
+        out.flush();
+        out.write( body + CRLF);
+        out.flush();
+
+        //Ending message
+        out.write("." + CRLF);
+        out.flush();
+        while ((line = in.readLine()) != null && !line.contains(ACCEPTED_REQUEST)) {}
+        }
+
+    private String utf8Formatting(String text) {
         return "=?utf-8?Q?" + text + "?=";
-    }
-    public static void main(String[] args) {
-        new Client().connect();
     }
 }
